3a9ad0d6bb40af61dcf6867cde8c1cb4e06b0b9d
Ryan Dittmer <rdittmer@fullsail.edu>
2012-02-23 20:14:08 -0500

    added json data

---
+++ json.js
@@ -0,0 +1,18 @@
+var json = {
+	"Bill1":{
+		"billTypes": ["Bill Type:", "Utilities"],
+		"payto":     ["Pay To:", "City of Jacksonville"],
+		"payamount": ["Pay Amount:", "60"],
+		"paywith":   ["Pay With:", "Bank Account"],
+		"paydate":   ["Pay Date:", "03/11/2012"],
+		"notes":     ["Notes:", "N/A"]
+	},
+	"Bill2":{
+		"billTypes": ["Bill Type:", "Auto"],
+		"payto":     ["Pay To:", "Navy Fed"],
+		"payamount": ["Pay Amount:", "271.00"],
+		"paywith":   ["Pay With:", "Credit Card"],
+		"paydate":   ["Pay Date:", "03/01/2012"],
+		"notes":     ["Notes:", "N/A"]
+	}
+}
